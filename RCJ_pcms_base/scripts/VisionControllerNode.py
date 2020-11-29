318201d41372d562d77967b2746bac2dc1ad3d8f
rootadminwalker <chiioleong519@gmail.com>
2020-11-29 21:38:20 +0800

    Rollback

--- RCJ_pcms_base/scripts/VisionControllerNode.py
+++ RCJ_pcms_base/scripts/VisionControllerNode.py
@@ -23,8 +23,7 @@ SOFTWARE.
 """
 
 from core.base_classes import Node
-from core.Nodes.Visions import YOLODetection
-from core.Nodes.Visions import vision_abstract
+from core.Nodes.Visions import YOLODetection, vision_abstract
 
 from home_robot_msgs.msg import VisionRequest
 from home_robot_msgs.srv import VisionDeregister, VisionDeregisterRequest, VisionDeregisterResponse
