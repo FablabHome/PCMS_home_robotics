ffbdd7205094d35ed955cb42404491af05eae478
rootadminwalker <chiioleong519@gmail.com>
2021-03-25 20:08:34 +0800

    Final core of PersonFollower

--- RCJ_pcms_base/scripts/PersonFollower.py
+++ RCJ_pcms_base/scripts/PersonFollower.py
@@ -5,60 +5,45 @@ import cv2 as cv
 import numpy as np
 import rospy
 from cv_bridge import CvBridge
-from geometry_msgs.msg import Twist
-from home_robot_msgs.msg import ObjectBoxes
+from home_robot_msgs.msg import ObjectBoxes, PFRobotData
+from home_robot_msgs.srv import PFInitializer, PFInitializerResponse
 from rospkg import RosPack
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 
 from core.Detection import PersonReidentification
 from core.Dtypes import BBox
-from core.tools import PIDController
 
 
 class PersonFollower:
     H = 480
     W = 640
     CENTROID = (W // 2, H // 2)
-
-    FORWARD_KP = 1 / 900
-    FORWARD_KD = 1 / 50
-
-    TURN_KP = 1 / 150
-    TURN_KD = 1 / 50
-
-    SMOOTH_CONTROL_KP = 1 / 5
-    SMOOTH_CONTROL_KD = 1 / 5
-
-    FORWARD_SPEED_LIMIT = 1.1
-
-    TARGET_DIST = 1080
+    SIMIL_ERROR = 0.7
 
     def __init__(self, person_extractor: PersonReidentification):
-        # rospy.Service('pf_initialize', PFInitializer, self.initialized_cb)
         self.person_extractor = person_extractor
-        self.target_box = None
-        self.rgb_image = self.depth_image = None
         self.bridge = CvBridge()
 
-        self.forward_controller = PIDController(
-            PersonFollower.FORWARD_KP, 0, PersonFollower.FORWARD_KD
-        )
-        self.turn_controller = PIDController(
-            PersonFollower.TURN_KP, 0, PersonFollower.TURN_KD
-        )
-        self.smooth_controller = PIDController(
-            PersonFollower.SMOOTH_CONTROL_KP, 0, PersonFollower.SMOOTH_CONTROL_KD
+        self.front_descriptor = self.back_descriptor = None
+
+        self.target_box = None
+        self.detection_boxes = []
+        self.max_distance = 0
+
+        self.rgb_image = None
+        self.initialized = False
+
+        rospy.Service('pf_initialize', PFInitializer, self.initialized_cb)
+
+        self.robot_handler_publisher = rospy.Publisher(
+            '/PFRHandler/pf_data',
+            PFRobotData,
+            queue_size=1
         )
 
-        self.forward_speed = 0
-        self.turn_speed = 0
-
-        # self.last_forward_speed = 0
-        # self.last_turn_speed = 0
-
-        self.twist_publisher = rospy.Publisher(
-            '/mobile_base/commands/velocity',
-            Twist,
+        self.image_publisher = rospy.Publisher(
+            '/PF/drown_image',
+            CompressedImage,
             queue_size=1
         )
 
@@ -68,59 +53,85 @@ class PersonFollower:
             self.box_callback,
             queue_size=1
         )
-        rospy.Subscriber(
-            '/camera/depth/image_raw',
-            Image,
-            self.depth_callback,
-            queue_size=1
-        )
         rospy.set_param('~lost_target', False)
+        self.main()
 
-    # def initialized_cb(self, req):
-    #     self.front_descriptor = np.array(req.front_descriptor)
-    #     self.back_descriptor = np.array(req.back_descriptor)
-
-    def depth_callback(self, depth: Image):
-        self.depth_image = self.bridge.imgmsg_to_cv2(depth)
+    def initialized_cb(self, req):
+        self.front_descriptor = np.array(req.front_descriptor)
+        self.back_descriptor = np.array(req.back_descriptor)
+        self.initialized = True
+        return PFInitializerResponse(True)
 
     def box_callback(self, detections: ObjectBoxes):
-        rospy.loginfo('hei')
-        detection_boxes = detections.boxes
+        if self.initialized:
+            self.detection_boxes = detections.boxes
+            # max_distance = 0
 
-        PersonFollower.H = detections.source_img.height
-        PersonFollower.W = detections.source_img.width
-        max_distance = 0
+            self.rgb_image = self.bridge.compressed_imgmsg_to_cv2(detections.source_img)
+            H, W, _ = self.rgb_image.shape
 
-        self.rgb_image = self.bridge.imgmsg_to_cv2(detection_boxes.source_img, 'bgr8')
-        for det_box in detection_boxes:
-            source_img = self.bridge.imgmsg_to_cv2(det_box.source_img, 'bgr8')
-            if det_box.label != 'person':
+            PersonFollower.H = H
+            PersonFollower.W = W
+            PersonFollower.CENTROID = (PersonFollower.W // 2, PersonFollower.H // 2)
+
+    def main(self):
+        while not rospy.is_shutdown():
+            self.max_distance = 0
+            srcframe = self.rgb_image
+            if srcframe is None:
                 continue
-            person_box = BBox(det_box.x1, det_box.y1, det_box.x2, det_box.y2, label='person',
-                              score=det_box.score,
-                              source_img=source_img)
-            distance_between_centroid = person_box.calc_distance_between_point(PersonFollower.CENTROID)
-            if max_distance < distance_between_centroid < 30:
-                max_distance = distance_between_centroid
-                self.target_box = person_box
 
-        self.target_box.draw(self.rgb_image, (32, 0, 255))
-        self.target_box.draw_centroid(self.rgb_image, (32, 0, 255), 5)
+            for det_box in self.detection_boxes:
+                source_img = self.bridge.compressed_imgmsg_to_cv2(det_box.source_img)
+                if det_box.label.strip() != 'person':
+                    continue
 
-        x = self.target_box.x1
-        centroid_x = PersonFollower.CENTROID[0]
-        distance = self.depth_image[self.target_box.centroid]
+                person_box = BBox(det_box.x1, det_box.y1, det_box.x2, det_box.y2, label='person',
+                                  score=det_box.score,
+                                  source_img=source_img)
+                current_descriptor = self.person_extractor.parse_descriptor(source_img)
+                distance_between_centroid = person_box.calc_distance_between_point(PersonFollower.CENTROID)
+                # rospy.loginfo(distance_between_centroid)
 
-        forward_error = distance - PersonFollower.TARGET_DIST
-        target_forward_speed = self.forward_controller.update(forward_error)
+                front_similarity = self.__compare_descriptor(current_descriptor, self.front_descriptor)
+                back_similarity = self.__compare_descriptor(current_descriptor, self.back_descriptor)
 
-        target_speed_error = target_forward_speed - self.forward_speed
-        smooth_speed = self.smooth_controller.update(target_speed_error)
-        self.forward_speed = min(smooth_speed, PersonFollower.FORWARD_SPEED_LIMIT)
+                if self.max_distance < distance_between_centroid < 250:
+                    if self.__similarity_lt(front_similarity) or self.__similarity_lt(back_similarity):
+                        # max_distance = distance_between_centroid
+                        self.target_box = person_box
 
-        turn_error = x - centroid_x
-        target_turn_speed = self.turn_controller.update(turn_error)
-        self.turn_speed = target_turn_speed
+                        self.target_box.draw(srcframe, (32, 255, 0), 7)
+                        self.target_box.draw_centroid(srcframe, (32, 0, 255), 5)
+                        break
+            else:
+                self.target_box = None
+
+            msg = PFRobotData()
+            if self.rgb_image is None or not self.initialized:
+                continue
+
+            if self.target_box is not None:
+                msg.follow_point = self.target_box.centroid
+            else:
+                msg.follow_point = (-1, -1)
+
+            self.robot_handler_publisher.publish(msg)
+
+            # drown_image = node.bridge.cv2_to_compressed_imgmsg(rgb_image)
+            # node.image_publisher.publish(drown_image)
+
+            frame = srcframe
+            cv.imshow('frame', frame)
+            key = cv.waitKey(16)
+            if key in [ord('q'), 27]:
+                break
+
+        cv.destroyAllWindows()
+
+    @staticmethod
+    def __similarity_lt(similarity):
+        return similarity > PersonFollower.SIMIL_ERROR
 
     @staticmethod
     def __compare_descriptor(desc1, desc2):
@@ -129,7 +140,6 @@ class PersonFollower:
 
 if __name__ == '__main__':
     rospy.init_node('test_person_follower')
-    rate = rospy.Rate(30)
     base = RosPack().get_path('rcj_pcms_base') + '/..'
     bin_path = path.join(
         base, 'models/intel/person-reidentification-retail-0277/FP32/person-reidentification-retail-0277.bin')
@@ -139,17 +149,25 @@ if __name__ == '__main__':
     person_descriptor_extractor = PersonReidentification(net)
     node = PersonFollower(person_descriptor_extractor)
 
-    while not rospy.is_shutdown():
-        twist = Twist()
-        forward_speed = node.forward_speed
-        turn_speed = node.turn_speed
-
-        twist.linear.x = forward_speed
-        twist.angular.z = turn_speed
-        node.twist_publisher.publish(twist)
-
-        if node.rgb_image is None:
-            continue
-
-        cv.imshow('frame', node.rgb_image)
-        cv.waitKey(1)
+#     while not rospy.is_shutdown():
+#         msg = PFRobotData()
+#         rgb_image = node.rgb_image
+#         if rgb_image is None or not node.initialized:
+#             continue
+#
+#         if node.target_box is not None:
+#             msg.follow_point = node.target_box.centroid
+#         else:
+#             msg.follow_point = (-1, -1)
+#
+#         node.robot_handler_publisher.publish(msg)
+#
+#         # drown_image = node.bridge.cv2_to_compressed_imgmsg(rgb_image)
+#         # node.image_publisher.publish(drown_image)
+#
+#         cv.imshow('frame', rgb_image)
+#         key = cv.waitKey(1)
+#         if key in [ord('q'), 27]:
+#             break
+#
+# cv.destroyAllWindows()
