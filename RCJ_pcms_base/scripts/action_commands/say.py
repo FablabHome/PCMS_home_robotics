3de116e8bf0b5a8a00c97ef7317d3cf413768147
rootadminwalker <chiioleong519@gmail.com>
2021-07-05 14:45:42 +0800

    add wait-until-end option

--- RCJ_pcms_base/scripts/action_commands/say.py
+++ RCJ_pcms_base/scripts/action_commands/say.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python3
 import argparse
-import pyttsx3
+import rospy
 import sys
 
-engine = pyttsx3.init()
+from mr_voice.srv import SpeakerSrv
+from std_msgs.msg import String
 
 
 def main(args):
-    global engine
-    engine.say(args['text'])
-    engine.runAndWait()
+    global pub, service
+    if args['wait_until_end']:
+        service(args['text'])
+    else:
+        pub.publish(args['text'])
 
 
 if __name__ == '__main__':
+    rospy.init_node('say', anonymous=True)
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-t', '--text', type=str, required=True, help='Text you want the computer to say')
     parser.add_argument('-r', '--rate', type=int, default=130, help='Speed of the speaker')
     parser.add_argument('-v', '--volume', type=float, default=1.0, help='Volume of the speaker')
     parser.add_argument('-l', '--language', type=str, default='en-us', help='Language of the speaker')
+    parser.add_argument('-w', '--wait-until-end', action='store_true')
 
     args = vars(parser.parse_args())
 
-    engine.setProperty('rate', args['rate'])
-    engine.setProperty('volume', args['volume'])
-    engine.setProperty('voice', args['language'])
-
+    pub = rospy.Publisher(
+        '/speaker/say',
+        String,
+        queue_size=1
+    )
+    service = rospy.ServiceProxy('/speaker/text', SpeakerSrv)
+    rospy.sleep(.5)
     try:
         main(args)
         sys.exit(0)
